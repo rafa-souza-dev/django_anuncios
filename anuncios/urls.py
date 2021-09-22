79e49ba8295a4736882224547ff7cab3705c34e9
Rafael <rafaeljosegomes25@hotmail.com>
2021-09-21 22:51:02 -0300

    criada função que cadastra anúncios

--- anuncios/urls.py
+++ anuncios/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import Home
+
+from .views import home, categoria_filtrada, anuncio_filtrado, AnuncioCreate, AnuncioUpdate
 
 urlpatterns = [
-    path('', Home, name='home')
+    path('', home, name='home'),
+    path('categoria/<int:categoria_id>', categoria_filtrada, name='categoria'),
+    path('anuncio/<int:anuncio_id>', anuncio_filtrado, name='anuncio'),
+    path('cadastrar/anuncio/', AnuncioCreate.as_view(), name='cadastrar-anuncio'),
+    #path('editar/anuncio/<int:pk>', AnuncioUpdate.as_view(), name='editar-anuncio'),
 ]
--- anuncios/views.py
+++ anuncios/views.py
@@ -1,5 +1,57 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from django.urls import reverse_lazy
+
+from anuncios import models
 
 
-def Home(request):
-    return HttpResponse("Olá Mundo!!")
+def home(request):
+    categorias = models.Categoria.objects.all()
+    ultimos_anuncios = models.Anuncio.objects.all()[:12]
+
+    return render(request, 'home.html', {
+        'categorias': categorias,
+        'ultimos_anuncios': ultimos_anuncios
+    })
+
+
+def categoria_filtrada(request, categoria_id):
+    categoria = get_object_or_404(models.Categoria, id=categoria_id)
+
+    categorias = models.Categoria.objects.all()
+
+    anuncios = models.Anuncio.objects.filter(categoria=categoria)
+
+    return render(request, 'home.html', {
+        'categorias': categorias,
+        'ultimos_anuncios': anuncios,
+        'categoria': categoria,
+    })
+
+
+def anuncio_filtrado(request, anuncio_id):
+    anuncio = get_object_or_404(models.Anuncio, id=anuncio_id)
+
+    categorias = models.Categoria.objects.all()
+
+    return render(request, 'anuncio.html', {
+        'categorias': categorias,
+        'anuncio': anuncio,
+    })
+
+
+class AnuncioCreate(generic.CreateView):
+    model = models.Anuncio
+    fields = ['titulo', 'descricao', 'preco', 'categoria']
+    template_name = 'form.html'
+    success_url = reverse_lazy('home')
+    extra_context = {'categorias': models.Categoria.objects.all()}
+
+
+class AnuncioUpdate(generic.UpdateView):
+    model = models.Anuncio
+    fields = ['titulo', 'descricao', 'preco', 'categoria']
+    template_name = 'form.html'
+    success_url = reverse_lazy('home')
+    extra_context = {'categorias': models.Categoria.objects.all()}
+
