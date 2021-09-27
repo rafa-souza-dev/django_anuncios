9e23b28a9926cea0190500d90dd69fc476ce8b06
Rafael <rafaeljosegomes25@hotmail.com>
2021-09-27 15:57:17 -0300

    Python Decouple

--- django_anuncios/settings.py
+++ django_anuncios/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import django_on_heroku
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g7az8!(^sadsdasda+yv7yti%0yk7-44#fadqvysh&ifmbzj)zf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool, default=False)
 
 ALLOWED_HOSTS = []
 
